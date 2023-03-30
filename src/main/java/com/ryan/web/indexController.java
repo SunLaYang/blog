457aa1bc5b94774f71da4a04a8c7f94d28c7d5ba
Ryan <tototwgod5@gmail.com>
2023-03-30 13:28:57 +0800

    調整設定檔

---
+++ src/main/java/com/ryan/web/indexController.java
@@ -0,0 +1,21 @@
+package com.ryan.web;
+
+import com.ryan.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class indexController {
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
+//        int i = 9/0;
+//        String blog = null;
+//        if(blog == null){
+//            throw new NotFoundException("部落格頁面不存在");
+//        }
+        System.out.println("-----------index-----");
+
+        return "index";
+    }
+}
